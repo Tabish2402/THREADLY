71840be9e6fdd2734c40ffa5115b413816ff7cca
Tabish2402 <yellowflash2402@gmail.com>
2025-10-23 19:13:16 +0100

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,43 @@ import express from "express";
 
 import {ENV}from "./config/env.js"
 import { connectDB } from "./config/db.js";
+<<<<<<< HEAD
 
+=======
+import{clerkMiddleware} from "@clerk/express";
+import { inngest,functions } from "./config/inngest.js"
+import{serve} from "inngest/express";
+>>>>>>> 86c4945 (auth implemented)
 const app=express();
+app.use(express.json());
+app.use(clerkMiddleware())
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
 app.get("/",(req,res) => {
+
     res.send("hellow world");
 });
+<<<<<<< HEAD
 app.listen(ENV.PORT,()=>{
     console.log("Server started on: ",ENV.PORT)
     connectDB();
 });
+=======
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port:", ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+startServer();
+export default app;
+>>>>>>> 86c4945 (auth implemented)
